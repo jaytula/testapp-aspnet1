8b4b9b0eb38452d2064d372b8c8f5523eece6401
Jay T <jaytula@gmail.com>
2023-01-11 22:30:57 -0800

    Send encrypted data in test1.sh

--- aspnetapp/test1.sh
+++ aspnetapp/test1.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-curl -X POST -H 'Content-Type: application/json' http://localhost:8080/AuthLogin --data '{"authdata": "abcdef"}'
+openssl rsautl -encrypt -inkey id_rsa -in data.txt -out encrypted_data.bin
+curl -X POST -H 'Content-Type: application/octet-stream' --data-binary "@encrypted_data.bin" http://localhost:8080/AuthLogin
